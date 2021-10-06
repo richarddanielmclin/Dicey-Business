d001638e92fe0ef3d84c3142dddfd1b7b9a061e2
unknown <richdanmac@gmail.com>
2021-10-06 17:26:35 -0500

    added alert to show sum/finished

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // getting button from doc
 let newDieBtn = document.getElementById('new-die-btn');
 let rollDieBtn = document.getElementById('roll-die-btn');
+let sumDieBtn = document.getElementById('sum-die-btn');
+let diceArr = [];
 
 // creating container
 let container = document.createElement('div')
 container.classList.add('container')
+container.id = 'container-id'
 document.body.appendChild(container)
 
 // setting class for creation of new die
@@ -16,65 +19,67 @@ class Die {
         this.div.style.width = '100px'
         this.div.style.backgroundColor = 'rgb(102, 202, 186)';
         container.appendChild(this.div)
+        diceArr.push(this);
 
-        // setting random number generator from 1-6
-        function getRandomNumber(min, max) {
-            min = Math.ceil(1);
-            max = Math.floor(7);
-            return Math.floor(Math.random() * (7 - 1) + 1)
-        }
 
         // assigning random numbers to div ids
-        let randomNumber = getRandomNumber()
-        this.div.id = randomNumber;
-        this.div.textContent = randomNumber;
+        this.value = getRandomNumber()
+        this.div.id = this.value;
+        this.div.textContent = this.value;
+        // this.val = this.div.id
 
         // setting numbers to their dice unicode values
-        if (randomNumber === 1) {
+        if (this.value === 1) {
             this.div.textContent = '\u2680'
-        } else if (randomNumber === 2) {
+        } else if (this.value === 2) {
             this.div.textContent = '\u2681'
-        } else if (randomNumber === 3) {
+        } else if (this.value === 3) {
             this.div.textContent = '\u2682'
-        } else if (randomNumber === 4) {
+        } else if (this.value === 4) {
             this.div.textContent = '\u2683'
-        } else if (randomNumber === 5) {
+        } else if (this.value === 5) {
             this.div.textContent = '\u2684'
-        } else if (randomNumber === 6) {
+        } else if (this.value === 6) {
             this.div.textContent = '\u2685'
         }
 
-        // setting Roll Die button to roll all dice when clicked
-        rollDieBtn.addEventListener('click', () => this.roll())
 
+        // setting Roll Die button to roll all dice when clicked
         this.div.addEventListener('click', () => this.roll())
+        rollDieBtn.addEventListener('click', () => this.roll())
     }
 
     // creating rolling method that will update the numbers on the dice
     roll() {
-        function getRandomNumber(min, max) {
-            min = Math.ceil(1);
-            max = Math.floor(7);
-            return Math.floor(Math.random() * (7 - 1) + 1)
-        }
-        let randomNumber = getRandomNumber()
-        this.div.id = randomNumber;
-        this.div.textContent = randomNumber;
 
-        if (randomNumber === 1) {
+        this.value = getRandomNumber()
+        this.div.id = this.value;
+        this.div.textContent = this.value;
+
+        if (this.value === 1) {
             this.div.textContent = '\u2680'
-        } else if (randomNumber === 2) {
+        } else if (this.value === 2) {
             this.div.textContent = '\u2681'
-        } else if (randomNumber === 3) {
+        } else if (this.value === 3) {
             this.div.textContent = '\u2682'
-        } else if (randomNumber === 4) {
+        } else if (this.value === 4) {
             this.div.textContent = '\u2683'
-        } else if (randomNumber === 5) {
+        } else if (this.value === 5) {
             this.div.textContent = '\u2684'
-        } else if (randomNumber === 6) {
+        } else if (this.value === 6) {
             this.div.textContent = '\u2685'
         }
     }
+}
+
+const sumDie = () => {
+    let sum = 0;
+    diceArr.forEach(die => {
+        // use die.value add on to sum
+        sum += die.value
+    })
+    alert(`The sum of the dice is ${sum}!`)
+
 }
 
 // new die every time button is clicked
@@ -82,3 +87,11 @@ newDieBtn.addEventListener('click', () => {
     new Die();
 })
 
+// setting random number generator from 1-6
+function getRandomNumber(min, max) {
+    min = Math.ceil(1);
+    max = Math.floor(7);
+    return Math.floor(Math.random() * (7 - 1) + 1)
+}
+
+sumDieBtn.addEventListener('click', () => sumDie())
